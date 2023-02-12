c616ff2954acf8a7971aed569be3a64250db571d
ZahraaTayyar <zahraatayyar@gmail.com>
2023-02-12 15:21:14 +0000

    challenge 01 completed

--- challenges/dog-photo-gallery/script.js
+++ challenges/dog-photo-gallery/script.js
@@ -1,4 +1,25 @@
+const genBtn = document.querySelector(".new");
+const clearBtn = document.querySelector(".clear");
+let list = document.querySelector("li");
 
-//global variables
-let buttonEl = document.createElement("button");
-body.appendChild(buttonEl);
+function getRandomDogImage() {
+    fetch("https://dog.ceo/api/breeds/image/random")
+        .then((res) => {
+            return res.json()
+        })
+        .then((data) => {
+            let imageEl = document.querySelector(".dog-image");
+            imageEl.src = data.message;
+
+            list.appendChild(imageEl);
+
+        })
+        .catch((error) => console.log(error));
+}
+
+genBtn.addEventListener("click", getRandomDogImage);
+
+clearBtn.addEventListener("click", function () {
+    list.innerHTML("");
+})
+
